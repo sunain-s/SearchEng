93d47a51615b9f1b1227a060f4a9846ba94a35b3
Asianguy123 <sas@drsyed.org>
2022-05-27 09:54:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Search
 
 # Performs PR algorithm and provides ranking for searched terms
+# ==> PR(A) = (1 - d) + d[PR(T1)/C(T1) + ..... + PR(Tn)/C(Tn)]
 # Utilises bs4's BeautifulSoup for html parsing
 
 def page_rank_algo():
