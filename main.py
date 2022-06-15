fb312e25de30c2910ca316aaf3a8c7f976095287
Asianguy123 <sas@drsyed.org>
2022-06-15 14:19:36 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,19 +16,8 @@ def main():
 d = 0.85
 htmls = [file for file in os.listdir() if file.endswith('.html')]
 page_ranks = [1 for i in range(len(htmls))]
+inbound_pages = [[] for i in range(len(htmls))] 
 outbound_counts = []
-inbound_pages = [
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-    [],
-]
 
 if __name__ == '__main__':
     main()
