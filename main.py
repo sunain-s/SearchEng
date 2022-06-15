6ae1a3acbba7ec5a7aa269df3e2f8d7216b10e5b
Asianguy123 <sas@drsyed.org>
2022-06-15 14:19:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def main():
   pass
 
 d = 0.85
-page_ranks = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+htmls = [file for file in os.listdir() if file.endswith('.html')]
+page_ranks = [1 for i in range(len(htmls))]
 outbound_counts = []
 inbound_pages = [
     [],
